093013163894a4e7f1857d152f13d988ac39a138
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-24 14:38:23 -0500

    Completed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let total = numbers.reduce((total, number) => {
+    return total + number;
+  }, 0)
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let total = numbers.reduce((total, number) => {
+    return total * number;
+  }, 1)
+  return total;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  let factorial = 1;
+  if (n === 0){
+    return factorial;
+  }
+  for (let i = 1; i <= n; i++) {
+      factorial *= i; 
+  }
+  return factorial;
 };
 
 module.exports = {
