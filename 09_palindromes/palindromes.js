e32e59bcb5a27027c132d7e70e3c8f9e0cf070d2
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-24 15:18:03 -0500

    Completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function(txt) {
+    txt = txt.toLowerCase();
+    let charArray = txt.split("");
+    //defines an array of specialCharacters to be filtered
+    let specialCharacters = [',', '.', '!', ':', ';', '?', '"', '\'', '¡', '¿', ' '];
+    //filters specialCharacters
+    let justLetters = charArray.filter(character => !(specialCharacters.includes(character)));
+    //reverses a copy of the array of letters 
+    let reversedJustLetters = [...justLetters].reverse();
+    for (let i=0; i < justLetters.length; i++){
+        //if only one word is different, then it isn't a palindrome
+        if (justLetters[i] !== reversedJustLetters[i]){
+            return false;
+        }
+    }
+    return true;
 };
+
 
 module.exports = palindromes;
