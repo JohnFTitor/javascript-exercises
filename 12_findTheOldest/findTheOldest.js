7dc5ecb7abb4dfce4e4777ec5986001c8812c541
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-24 16:39:10 -0500

    Completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(function (previous, next) {
+        let currentYear = new Date().getFullYear(); 
+        let previousAge = (("yearOfDeath" in previous) ? previous.yearOfDeath : currentYear) - previous.yearOfBirth;
+        let nextAge = (("yearOfDeath" in next) ? next.yearOfDeath : currentYear) - next.yearOfBirth;
+        return previousAge > nextAge ? -1 : 1;
+    })
+    return people[0];
 };
 
 module.exports = findTheOldest;
