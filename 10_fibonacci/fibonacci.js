5b19a9da788576e8622edf3b0e47c2bc3033639f
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2021-08-24 15:58:33 -0500

    Completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = +n;
+    if (n == 1 || n == 2) {
+        return 1;
+    } else if ( n <= 0) {
+        return "OOPS";
+    }
+    let previus = 1;
+    let next = 1;
+    for (let i = 3; i <= n; i++) {
+        let temporal = next;
+        next += previus;
+        previus = temporal;
+    }
+    return next; 
 };
 
 module.exports = fibonacci;
